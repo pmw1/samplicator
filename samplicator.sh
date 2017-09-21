d73c442697630c8b1c0f4c8648fe04c92b16aa12
root <root@ip-172-31-13-144.ec2.internal>
2017-09-21 05:21:28 +0000

    first commit

---
+++ samplicator.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+echo "running samplicator docker: 10.0.10.11"
+
+sudo docker kill samplicator
+sudo docker rm -f samplicator
+
+
+
+sudo docker run \
+	-i -t \
+	-v $HOME/apps/samplicator/hostfiles/:/hostfiles \
+	-p 3001:3001/udp \
+	--name="samplicator" \
+	--network="docker-network" \
+	--ip="10.0.10.2" \
+	--entrypoint="/bin/bash" \
+	pmw1/samplicator
